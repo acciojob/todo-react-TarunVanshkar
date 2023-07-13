ee4ce09497678e9dfeb8d65c0ea2fef83676893b
TarunVanshkar <tarunben33@gmail.com>
2023-07-13 21:59:48 +0530

    submitting code-Thu Jul 13 2023 21:59:35 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [id, setId] = useState(0);
+
+  function addTodo() {
+    setId(id + 1);
+    let todoObj = { 'id': id, 'todo': todo };
+    setTodoList([...todoList, todoObj]);
+  }
+
+  function deleteTodo(id) {
+    setTodoList(todoList.filter((todoItem) => todoItem.id !== id));
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="todo-container">
+      {/* Do not remove the main div */}
+      <h1>To-Do List</h1>
+      <div className="input">
+        <input type="text" onChange={(event) => setTodo(event.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+
+      <ul>
+        {
+          todoList.length > 0 &&
+          todoList.map((todoItem) => {
+            return (
+              <div className="item">
+                <li>{todoItem.todo}</li>
+                <button onClick={() => deleteTodo(todoItem.id)}>Delete</button>
+              </div>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
